8f159f42b3de3625030c639e80fa0ef8b7c22a81
Baqir  zafar farooqui <75995372+baqarzafar@users.noreply.github.com>
2025-10-26 00:17:32 -0700

    I have added backend

--- myshopparadise/src/App.js
+++ myshopparadise/src/App.js
@@ -1,8 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Content from './componts/contents';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+
+      fetch("http://localhost:3000/").then((res ,req)=>{console})
+
+
+  } )
   return (
     <div className="App" style={{width:"100%"}}>
           <Content></Content>
